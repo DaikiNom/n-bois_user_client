8986631a3423c8036b058413f71f3ac5315a7e03
Romanhikoh <soleil.aimer@gmail.com>
2023-12-24 19:51:58 +0900

    地図描画
    リアルタイムで地図を描画できるよう更新

--- lib/screen/map.dart
+++ lib/screen/map.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class BusMap extends StatefulWidget {
   const BusMap({Key? key}) : super(key: key);
@@ -8,15 +12,88 @@ class BusMap extends StatefulWidget {
 }
 
 class _BusMapState extends State<BusMap> {
+  // 位置情報のアップデートをlisten
+  late final Stream<List<BusLocation>> _busLocationStream;
+  final client = Supabase.instance.client;
+
+  final _markers = <Marker>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _busLocationStream = client
+        .from('bus_location')
+        .stream(primaryKey: ['busId']).map((maps) => maps
+            .map((map) => BusLocation.fromMap(
+                  map: map,
+                  id: map['busId'],
+                ))
+            .toList());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('N-BOIS'),
       ),
-      body: const Center(
-        child: Text('BusMap'),
+      body: StreamBuilder<List<BusLocation>>(
+        stream: _busLocationStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('エラーが発生しました'));
+          } else if (snapshot.hasData) {
+            _markers.clear();
+            for (final busLocation in snapshot.data!) {
+              if (busLocation.latitude <= 90 &&
+                  busLocation.latitude >= -90 &&
+                  busLocation.longitude <= 180 &&
+                  busLocation.longitude >= -180) {
+                _markers.add(
+                  Marker(
+                    width: 80.0,
+                    height: 80.0,
+                    point: LatLng(busLocation.latitude, busLocation.longitude),
+                    child: const Icon(
+                      Icons.directions_bus,
+                      color: Colors.blue,
+                    ),
+                  ),
+                );
+              }
+            }
+            return FlutterMap(
+              options: const MapOptions(
+                initialCenter: LatLng(35.652832, 139.839478),
+                initialZoom: 10.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                ),
+                MarkerLayer(
+                  markers: _markers,
+                ),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
+}
+
+class BusLocation {
+  final latitude;
+  final longitude;
+  final String id;
+
+  BusLocation.fromMap({
+    required Map<String, dynamic> map,
+    required this.id,
+  })  : latitude = map['latitude'],
+        longitude = map['longitude'];
 }
